02658a9c2e09b633c363e3d0bc812922d8ee3239
JohnMark13 <mark@soonami.io>
2022-05-29 16:52:51 +0100

    Reinstating code, won't work as I do not have permission

--- handlers/setAddress.js
+++ handlers/setAddress.js
@@ -4,9 +4,30 @@ const configs = require("../config.js")
 const commandConstants = require("../commandConstants.js")
 
 module.exports = {
-    setAddressHandler: (intData) => {
-        console.log("We've got: " + intData.id + " : " + intData.options.name + " : " + intData.options.value);
-        //{"guild_id":"956551108061446244","id":"979772431956861018","name":"setpayoutaddress","options":[{"name":"address","type":3,"value":"Dave"}],"type":1}
-        return "allGood";
+    setAddressHandler: (intMember, intData) => {
+        console.log("We've got: " + intData.id + " : " + intData.options[0].name + " : " + intData.options[0].value);
+        const address = intData.options[0](commandConstants.SET_ADDRESS_PARAM)
+        const config = configs.GUILD_IDS[interaction.guildId]
+
+        if (!config) {
+            return "Self ativation not correctly configured for this server, check guild setup";
+        }
+
+        try {
+            const ledgerManager = new sc.ledger.manager.LedgerManager({storage: new sc.ledger.storage.WritableGithubStorage({apiToken: process.env.GITHUB_SECRET, repo: config.repo, branch: config.branch})})
+            await ledgerManager.reloadLedger()
+            const discordAddress = getDiscordAddressFromId(intMember.user.id, false)
+            const account = ledgerManager.ledger.accountByAddress(discordAddress)
+            const uuid = account.identity.id
+            ledgerManager.ledger.setPayoutAddress(uuid, address, config.chainId, config.tokenAddress)
+            const result = await ledgerManager.persist()
+            if (result.error) {
+                return "Could not save address: " + result.error.toString();
+            }
+
+            return `Success!\n**New payout address:** ${address}\n**Account:** ${account.identity.name}\n**Chain Id:** ${config.chainId}\n**Token Address:** ${config.tokenAddress}`;
+        } catch (e) {
+            return `Failed with message: ` + e;
+        }
     }
 }
