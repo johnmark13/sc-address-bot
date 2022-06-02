0d5a10af5bc42a383280ba091ed726b93831f3cf
JohnMark13 <mark@soonami.io>
2022-06-02 09:27:10 +0100

    Updating config for test server

--- config.js
+++ config.js
@@ -7,5 +7,12 @@ module.exports = {
             chainId: "1", 
             tokenAddress: "0x333a4823466879eef910a04d473505da62142069"
         },
+        "981832725902278657": {
+            repo: "nation3/nationcred-instance", 
+            branch: "activation-bot-test", 
+            discordBaseURL: "https://forum.nation3.org",
+            chainId: "1", 
+            tokenAddress: "0x333a4823466879eef910a04d473505da62142069"
+        },
     },
 }
